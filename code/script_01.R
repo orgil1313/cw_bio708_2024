5d6a7a97ddfe5865312159c94e992a2b52ce6e8a
orgil1313 <m_sainzorig@uncg.edu>
2024-08-22 10:00:56 -0400

    Update script_01.R

--- code/script_01.R
+++ code/script_01.R
@@ -1,1 +1,13 @@
+# Biostat I testing code
+
 c(1,2)
+
+
+## produce 100 random numbers that follows a normal distribution
+x <- rnorm(n=100, mean = 0, sd = 1)
+
+## estimate mean
+mean(x)
+
+## estimate SD
+sd(x)
